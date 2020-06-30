be1b00fe7765e2e419f637de692c7d02583b3e55
Glitch (hello-express) <none>
2020-06-30 19:23:44 +0000

    🛩📮 Checkpoint ./server.js:18318225/3800

--- server.js
+++ server.js
@@ -11,12 +11,15 @@ const { error } = require('console');
 const accountSid = 'AC512a7091ec17836bbc605e4132e29ac7';
 const authToken = '09d81a7088b82e9acb9131767fe74e3d';
 const client = require('twilio')(accountSid, authToken);
-
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, 'assets')));
 app.use(express.json({limit:'1mb'}))
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/new5.htm'));
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/new5.html");
 });
 
 app.post('/sendMessage',function (req,res) {
@@ -26,37 +29,9 @@ app.post('/sendMessage',function (req,res) {
     client.messages
       .create(
           {
-            body: '# '+sendmessage,
+            body: '# '+sendsmessage,
             from: '+12024107229', 
-            to: '+919960085483'
+            to: .ev
         })
       .then(message => console.log(message.sid));
 })
-app.listen(8080);const app = express();
-
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
