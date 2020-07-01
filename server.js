ff6cb5232c21156bd5687fb5762201c0cdf9e850
Glitch (whip-troubled-cafe) <none>
2020-07-01 08:57:08 +0000

    👀🛫 Checkpoint ./server.js:18318225/395 ./views/index.html:18318225/12

--- server.js
+++ server.js
@@ -15,19 +15,22 @@ app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "assets")));
 app.use(express.json({ limit: "1mb" }));
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/login.htm");
+  response.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/geofence/:latlng",async (req,res)=>{
+app.get("/geofence/:latlng", (req,res)=>{
   const layerID = 'BIRLACROSS';
   const HERE_API_KEY = process.env.HERE_API_KEY;
   let proximity = req.params.latlng;
   console.log(proximity)
   let keyAttribute = 'NAME'
-  apiURL = `https://fleet.ls.hereapi.com/2/search/proximity.json?${layerID}&apikey=${HERE_API_KEY}&proximity=${proximity}&key_attribute=${keyAttribute}`
-  const data = await fetch(apiURL,{method:'GET'})
-  const fenceData = await data.json()
-  res.json(fenceData)
+
+  const apiURL = `https://fleet.ls.hereapi.com/2/search/proximity.json?${layerID}&apikey=${HERE_API_KEY}&proximity=${proximity}&key_attribute=${keyAttribute}`
+  const data =  fetch(apiURL,{method:'GET'}).then(res=>{
+    const fenceData =  data.json()
+  }).then(fenceData=>{
+    res.send(fenceData)
+  })
 })
 
 
